72891832f17b3d5c80072defa08a43ac55f2cdd1
maksym.perevalov <maksym.perevalov@sigma.software>
2024-05-22 23:01:49 +0300

    Initial commit

---
+++ src/main/kotlin/example/com/solidgatetask/SolidgateTaskApplication.kt
@@ -0,0 +1,11 @@
+package example.com.solidgatetask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SolidgateTaskApplication
+
+fun main(args: Array<String>) {
+	runApplication<SolidgateTaskApplication>(*args)
+}
---
+++ src/test/kotlin/example/com/solidgatetask/SolidgateTaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package example.com.solidgatetask
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SolidgateTaskApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
