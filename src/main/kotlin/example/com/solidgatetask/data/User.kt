a70539be9a316b1a298048b588f6853d0ec191e6
maksym.perevalov <maksym.perevalov@sigma.software>
2024-05-24 00:35:53 +0300

    Use proper JPA equals and hashcode for User entity

--- src/main/kotlin/example/com/solidgatetask/data/User.kt
+++ src/main/kotlin/example/com/solidgatetask/data/User.kt
@@ -3,7 +3,20 @@ package example.com.solidgatetask.data
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import org.hibernate.Hibernate
+import org.hibernate.proxy.HibernateProxy
 
 @Entity
 @Table(name = "users")
-class User(@Id var id: Int?, var name: String, var balance: Int)
+class User(@Id var id: Int?, var name: String, var balance: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        other as User
+        return id != null && id == other.id
+    }
+
+    override fun hashCode(): Int = 1756406093
+
+    override fun toString() = "User(id=$id, name='$name', balance=$balance)"
+}
